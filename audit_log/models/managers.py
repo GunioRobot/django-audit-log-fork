28d4f4bb0212522cb4a66673266d1519adbe007c
CNBorn <CNBorn@hotmail.com>
2010-07-01 16:55:23 +0800

    get_diff refactor and it can display action status.

--- audit_log/models/managers.py
+++ audit_log/models/managers.py
@@ -44,34 +44,33 @@ class AuditLogManager(models.Manager):
         model2 = LogEntry_list[1].object_state
 
         changes = {}
-        excludes = [] #should have a _meta.do_not_show_diff_field or something.
-    
-        for field in model1._meta.fields:
-            if not field.name in excludes:
-                if field.value_from_object(model1) != field.value_from_object(model2):
-                    try:
-                        changes[field.verbose_name] = (field.value_from_object(model2).encode("utf-8"), \
-                                                       field.value_from_object(model1).encode("utf-8"))
-                    except:
-                        changes[field.verbose_name] = (str(field.value_from_object(model2)), \
-                                                       str(field.value_from_object(model1)))
-        for field in model1._meta.local_many_to_many:
-            if not field.name in excludes:
-                if field.value_from_object(model1) != field.value_from_object(model2) and \
-                   str(field.value_from_object(model1)) != str(field.value_from_object(model2)):
-                    try:
-                        changes[field.verbose_name] = (field.value_from_object(model2).encode("utf-8"), \
-                                                       field.value_from_object(model1).encode("utf-8"))
-                    except:
-                        changes[field.verbose_name] = (str(field.value_from_object(model2)), \
-                                                       str(field.value_from_object(model1)))
+        changes_header = {}
+        excludes = ['edittime'] #should have a _meta.do_not_show_diff_field or something.
+   
+        changes_header['Modified'] = str(LogEntry_list[0].action_date)
+        changes_header['User'] = str(LogEntry_list[0].action_user)
+        changes_header['Type'] = str(LogEntry_list[0].action_type)
 
+        if changes_header['Type'].lower() == 'u': #if Changed
+
+            field_joint  = model1._meta.fields + model1._meta.local_many_to_many
+
+            for field in field_joint:
+                if not field.name in excludes:
+                    if field.value_from_object(model1) != field.value_from_object(model2) and \
+                       str(field.value_from_object(model1)) != str(field.value_from_object(model2)):
+                        try:
+                            changes[field.verbose_name] = (field.value_from_object(model2).encode("utf-8"), \
+                                                           field.value_from_object(model1).encode("utf-8"))
+                        except:
+                            changes[field.verbose_name] = (str(field.value_from_object(model2)), \
+                                                           str(field.value_from_object(model1)))
         try:
             context = ", ".join(map(lambda x:u"%(k)s:%(o)s->%(n)s" % {'k':x[0],'o':x[1][0],'n':x[1][1]}, changes.iteritems()))
         except:
             context = ", ".join(map(lambda x:u"%(k)s:%(o)s->%(n)s" % {'k':str(x[0]).decode("utf-8"),'o':str(x[1][0]).decode("utf-8"),'n':str(x[1][1]).decode("utf-8")}, changes.iteritems())) 
 
-        return context
+        return changes_header, context
             
 class AuditLogDescriptor(object):
     def __init__(self, model, manager_class):
